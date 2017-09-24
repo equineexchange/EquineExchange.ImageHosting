8538d65369f10ca3dab37ddcf607956b5d55897b
Brian Surowiec <xtorted@optonline.net>
2017-09-23 21:35:28 -0400

    Add application version telemetry initializer

---
+++ EquineExchange.ImageHosting/ApplicationInformation.cs
@@ -0,0 +1,16 @@
+﻿namespace EquineExchange.ImageHosting
+{
+    using System.Reflection;
+
+    public static class ApplicationInformation
+    {
+        static ApplicationInformation()
+        {
+            var assembly = typeof(ApplicationInformation).Assembly;
+
+            Version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        }
+
+        public static string Version { get; }
+    }
+}
---
+++ EquineExchange.ImageHosting/ApplicationInsights/ApplicationVersionTelemetryInitializer.cs
@@ -0,0 +1,13 @@
+﻿namespace EquineExchange.ImageHosting.ApplicationInsights
+{
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.Extensibility;
+
+    public class ApplicationVersionTelemetryInitializer: ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Component.Version = ApplicationInformation.Version;
+        }
+    }
+}
